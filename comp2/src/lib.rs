c06eac16d876213b71620a5de21d234701cab6ef
NewGyu <newgyu+git@kinoboku.net>
2023-08-29 05:54:26 +0000

    add `use` for generated component bindings.

--- comp2/src/lib.rs
+++ comp2/src/lib.rs
@@ -1,12 +1,21 @@
 cargo_component_bindings::generate!();
 
-use bindings::Example;
-
+// Generated binding method for `rand` function that is defined in comp1
+use bindings::comp1::rand;
+// Generated types that `rand` function depends on
+use bindings::newgyu::comp1::types::{Algorithm, Seed};
+// Generated for `hello-world` function that is defined in comp2/world.wit
+use bindings::Hello;
 struct Component;
 
-impl Example for Component {
+impl Hello for Component {
     /// Say hello!
     fn hello_world() -> String {
-        "Hello, World!".to_string()
+        let seed = Seed {
+            algorithm: Algorithm::Goblin,
+            value: 9,
+        };
+        let n = rand(seed);
+        format!("Hello, {}", n)
     }
 }
